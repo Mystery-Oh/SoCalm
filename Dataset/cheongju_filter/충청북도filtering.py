4d2f8d978c94da2f63f22024b3cda327dc44bfcb
장현규 <gusrb0117@naver.com>
2025-04-24 22:18:16 +0900

    간단한 주석 오타 수정

--- Dataset/cheongju_filter/충청북도filtering.py
+++ Dataset/cheongju_filter/충청북도filtering.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-# CSV 파일 불러오기. 공공데이터, 관공서는 cp949로 인코딩 경우가 많음
+# 원본 CSV 파일 불러오기.
 df = pd.read_csv('accident/지자체별_사고다발통계.csv', encoding='cp949')
 
 # '시도시군구명' 열에서, '충청북도 청주시'가 포함된 행 필터링
